485c89916755a985697b08c81c3e49e98452eb99
User <user@Users-MacBook-Pro.local>
2019-10-16 09:58:30 +0300

    notes_v1 initial commit

---
+++ notes_v1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Blog from './Blog/Blog';
+
+function App() {
+  return (
+    <div className="App">
+      <Blog article_count="14">here is the title</ Blog>
+    </div>
+  );
+}
+
+export default App;
---
+++ notes_v1/src/Blog/Blog.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const blog = (props) => {
+    return <p> Weelcome, here will be {props.article_count} articles to blog</p>
+};
+
+export default blog;
